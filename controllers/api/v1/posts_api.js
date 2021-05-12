274001fb780785da922636e1c140b4e8d933d8d7
chandan0-1 <chandanofficial.cn@gmail.com>
2021-05-12 20:43:21 +0530

    Accessing and deleting posts via APIs

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,41 @@
-module.exports.index = function(req,res){
+let Post = require('../../../models/post');
+let Comment = require('../../../models/comment');
+
+module.exports.index = async function (req, res) {
+
+  let posts = await Post.find({})
+    .populate('user')
+    .populate({
+      path: 'comments',
+      populate: {
+        path: 'user'
+      }
+    });
+
   return res.json(200, {
-    message : "List of Posts",
-    posts: []
+    message: "List of Posts",
+    posts: posts
   })
+}
+
+module.exports.destroy = async function (req, res) {
+
+  try {
+    let post = await Post.findById(req.params.id);
+
+    // if (post.user == req.user.id){
+    post.remove();
+
+    await Comment.deleteMany({ post: req.params.id });
+    return res.json(200, {
+      message: 'post and all asociated comments are deleted successfully'
+    });
+  }
+  catch (err) {
+    console.log(err)
+    return res.json(500, {
+      message: 'Internal Server Error'
+    });
+  }
+
 }
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const postApi = require('../../../controllers/api/v1/posts_api');
 
 router.get('/', postApi.index);
+router.delete('/delete/:id', postApi.destroy);
 
 module.exports = router;
