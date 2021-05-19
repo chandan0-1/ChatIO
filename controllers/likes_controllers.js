3dbc500321a9206d30292fbeb42df2c7e555bc48
chandan0-1 <chandanofficial.cn@gmail.com>
2021-05-19 08:37:48 +0530

    Created likes controller and Scehema

---
+++ controllers/likes_controllers.js
@@ -0,0 +1,57 @@
+const Like = require('../models/like');
+const Comment = require('../models/comment')
+const Post = require('../models/post');
+
+module.exports.toggleLike = async function(req, res){
+  try{
+    //like/toggle/?id=abcd&type=post
+    let likeable;
+    let deleted = false;
+
+    if (req.query.type == "Post"){
+      likeable = await Post.findById(req.query.id).populate('likes');
+    }
+    else{
+      likeable = await Comment.findById(req.query.id).populate('likes');
+    }
+
+
+    // Checking if likes already Exists
+    let existingLike = await like.findOne({
+      likeable: req.query.id,
+      onModel: req.query.type,
+      user: req.query._id
+    })
+
+    // if a like is already exists then deleting it
+    if (existingLike){
+      likeable.likes.pull(existingLike._id);
+      likeable.save();
+      existingLike.remove();
+      deleted = true;
+    }else{
+      // else creating a like
+      let newLike = await Like.create({
+        user: req.user.id,
+        likeable: req.query.id,
+        onModel: req.query.type
+      })
+
+      likeable.likes.push(like.id);
+      likeable.save();
+    }
+
+    return res.json(200, {
+      message: "Request is Succesfull",
+      data:{
+        deleted: deleted
+      }
+    })
+
+  }catch(err){
+    console.log(err);
+    return res.json(500, {
+      message: "Internal Server Error"
+    }
+  }
+}
--- models/post.js
+++ models/post.js
@@ -1,25 +1,32 @@
 const mongoose = require("mongoose")
 const postScheme = new mongoose.Schema({
-  content:{
-    type:String,
-    required:true
+  content: {
+    type: String,
+    required: true
   },
-  user:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User"
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
   },
 
   // Include the array of ids of all comments in the post schema itself
-  comments:[
+  comments: [
     {
-      type:mongoose.Schema.Types.ObjectId,
-      ref:"Comment"
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ],
+  likes:[
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Like'
     }
   ]
-},
-{
-  timestamps:true
-});
 
-const Post = mongoose.model('Post',postScheme);
+},
+  {
+    timestamps: true
+  });
+
+const Post = mongoose.model('Post', postScheme);
 module.exports = Post;
